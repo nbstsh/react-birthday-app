a702b8ec8004a907cf28f77fcc6ed786eca4d0c6
nbstsh <nbstsh@gmail.com>
2019-04-06 05:54:44 +0900

    Add functionality to remove person when the button is clicked in PersonDetailHeader Component

--- src/components/person-detail/PersonDetailHeader.js
+++ src/components/person-detail/PersonDetailHeader.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react'
 import BirthdayEditFormModalControll from './BirthdayEditFormModalControll'
+import manager from '../../js/person-manager';
 
 class PersonDetailHeader extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            needShowDeleteBtn: false
+        }
+    }
+    toggleDeleteBtn = () => {
+        this.setState({ needShowDeleteBtn: !this.state.needShowDeleteBtn })
+    }
+    handleDeleteBtnCoverClick = (e) => {
+        e.stopPropagation()
+        this.toggleDeleteBtn()
+    } 
+    handleDeleteBtnClick = () => {
+        manager.removePerson(this.props.person.id)
+        this.props.handleReturnBtn()
     }
     render() {
+        const coverStatus = this.state.needShowDeleteBtn ? 'remove' : ''
         return (
             <div className='person-detail__header'>
-                <span className='person-detail__button' onClick={this.props.handleReturnBtn}><i className="fas fa-chevron-left"></i></span>
+
+                <span 
+                    className='person-detail__button' 
+                    onClick={this.props.handleReturnBtn}>
+                    <i className="fas fa-chevron-left"></i>
+                </span>
+
                 <p className='person-detail__name'>{this.props.person.name}</p>
-                <BirthdayEditFormModalControll person={this.props.person}/>
-                <span className='person-detail__button'><i className="fas fa-trash-alt"></i></span>
+
+                <BirthdayEditFormModalControll 
+                    person={this.props.person}/>
+
+                <span 
+                    className='person-detail__button'
+                    onClick={this.handleDeleteBtnClick}>
+                    <span 
+                        className={`person-detail__button-cover ${coverStatus}`}
+                        onClick={this.handleDeleteBtnCoverClick}></span>
+                    <i className="fas fa-trash-alt"></i>
+                </span>
             </div>
         ) 
     }
