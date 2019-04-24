86d0ded7bf700388f1177d97602f35d7ec4ccfc6
nbstsh <nbstsh@gmail.com>
2019-04-25 03:28:06 +0900

    Reset button style

--- src/components/Signin/SignupDescription.js
+++ src/components/Signin/SignupDescription.js
@@ -3,7 +3,7 @@ import React from 'react'
 const SignupDescription = () => {
     return (
         <React.Fragment>
-            <p className='auth__description'>This app allow you to register birthday of your friends, family, or somebody important to you. You can also write memos about the person. Enjoy adding birthday card!</p>
+            {/* <p className='auth__description'>This app allow you to register birthday of your friends, family, or somebody important to you. You can also write memos about the person. Enjoy adding birthday card!</p> */}
             <p className='auth__description'>You can use this app without sign up. But you might lose the data if your brower clean it up. If you don't want it to happen, please sign up! Once you sign up, your data is going to be stored in cloud service and you don't need to worry about forgetting birthdays anymore! </p>
         </React.Fragment>
     )
